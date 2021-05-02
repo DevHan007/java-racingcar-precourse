1cde6e7dcc56512b3db7c353166877372ebc337c
DevHan007 <wjdgns2619@naver.com>
2021-05-03 01:50:55 +0900

    feat : 자동차 이름, 이동 LAPS 입력 기능 추가

--- src/main/java/com/racing/service/RacingService.java
+++ src/main/java/com/racing/service/RacingService.java
@@ -72,4 +72,18 @@ public class RacingService {
         }
         return winners;
     }
+
+    public static int getTrackCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        Scanner scanner = new Scanner(System.in);
+        String inputNumber = scanner.nextLine();
+        return Integer.parseInt(inputNumber.trim());
+    }
+
+    public static String getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)기준으로 구분)");
+        Scanner scanner = new Scanner(System.in);
+        String inputCarName = scanner.nextLine();
+        return inputCarName.replace(" ", "");
+    }
 }
