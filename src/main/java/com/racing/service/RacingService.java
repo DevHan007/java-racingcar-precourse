539e81ff617fdcb2dbcef87011e76bfc1b3138dc
DevHan007 <wjdgns2619@naver.com>
2021-05-03 00:51:41 +0900

    feat : 분리된 자동차 이름으로 최초 자동차 객체 생성 기능 추가

--- src/main/java/com/racing/service/RacingService.java
+++ src/main/java/com/racing/service/RacingService.java
@@ -1,5 +1,8 @@
 package com.racing.service;
 
+import com.racing.car.Car;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,4 +12,12 @@ public class RacingService {
         return Arrays.asList(inputCarNames.split(","));
     }
 
+    public List<Car> createRacingGameCars(List<String> inputCarNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : inputCarNames) {
+            Car car = new Car(carName, 0);
+            cars.add(car);
+        }
+        return cars;
+    }
 }
--- src/test/java/com/racing/service/RacingServiceTest.java
+++ src/test/java/com/racing/service/RacingServiceTest.java
@@ -1,5 +1,6 @@
 package com.racing.service;
 
+import com.racing.car.Car;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +21,18 @@ class RacingServiceTest {
                 .contains("1234")
                 .contains("12354")
                 .contains("1235");
+    }
+
+    @Test
+    @DisplayName("분리된 이름으로 각각의 자동차객체 최초생성 테스트")
+    void createRacingGameCars() {
+        RacingService racingService = new RacingService();
+        List<String> divideCarNames = racingService.divideCarNames("123,1234,12354,1235");
+        List<Car> resultCarNames = racingService.createRacingGameCars(divideCarNames);
+        assertThat(resultCarNames)
+                .hasSize(4)
+                .extracting("carName")
+                .contains("123", "1234", "12354", "1235");
     }
 
 }
